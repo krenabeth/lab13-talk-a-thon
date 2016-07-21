075da5f25a3eecfae7bc50578fbf932ab28de8e8
John Benson <johnbenson@gmail.com>
2016-07-21 19:33:48 -0400

    first 2 functions working after using objects

--- scripts.js
+++ scripts.js
@@ -8,31 +8,26 @@ app.controller('talkCtrl', function($scope){
       $scope.words = ['Mom!', 'Maaahhhhuuuuummm', 'dinosaurs', 'monster trucks', 'chicken nuggets'];
       $scope.words_dec = ['red', 'orange', 'yellow', 'green', 'blue']; // these will become SCSS classes
       $scope.phrase = ['he hit me', 'are we there yet?', "where's my tablet?!", "i'm hungry"];
-      $scope.phrase_dec = ['red', 'orange', 'yellow', 'green', 'blue'];
-
-      var chatterArray = [];
+      // just use words_dec to apply classes to phrase
+      $scope.chatterArray = [];
 
     // create scope function to push words to div
 
       $scope.addWords = function(){
-
-        var randomIndex;
-        // check length of array
-        var arrLength = $scope.words.length;
-        // pick a random number between 0 and length of array and store in var
-
-          function getRandomInt(min, max){
-            randomIndex = Math.floor(Math.random() * (max - min)) + min;
-          }
-        getRandomInt(0, arrLength);
-        console.log('random index is: ' + randomIndex);
-        // append that arrayWords indexOf[thatVariable] to the div
-        chatterArray.push($scope.words[randomIndex]);
-        console.log(chatterArray);
-        // need to properly define scope.chatter so it shows up on html
-        // $scope.chatter = 
+        // take a word from the scope.words array
+        var wordObj = {};
+        wordObj.name = $scope.words[Math.floor(Math.random() * $scope.words.length)];
+        wordObj.decoration = '';
+        $scope.chatterArray.push(wordObj);
       };
 
+      $scope.addWordsDec = function (){
+        var wordObj = {};
+        wordObj.name = $scope.words[Math.floor(Math.random() * $scope.words.length)];
+        wordObj.decoration = $scope.words_dec[Math.floor(Math.random() * $scope.words_dec.length)];
+        $scope.chatterArray.push(wordObj);
+        console.log(wordObj);
+      }
 
     // create scope function to push words_dec to div
 
